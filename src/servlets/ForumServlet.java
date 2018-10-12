bb875c0bc2d67e1f70d22c12fb5d4eaa89bb3c48
Andrey Ryabov <andreyryabov90gmail.com>
2018-10-13 01:02:58 +0300

    Commit +JSP

--- src/servlets/ForumServlet.java
+++ src/servlets/ForumServlet.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
@@ -18,7 +17,7 @@ import java.util.ArrayList;
 @WebServlet({"/forum"})
 public class ForumServlet extends HttpServlet {
 
-    static void TableCod(HttpServletRequest req, HttpServletResponse resp) throws UnsupportedEncodingException {
+    private static void TableCod(HttpServletRequest req, HttpServletResponse resp) throws UnsupportedEncodingException {
         req.setCharacterEncoding( "UTF-8" );
         resp.setCharacterEncoding( "UTF-8" );
         resp.setContentType( "text/html" );
@@ -38,36 +37,25 @@ public class ForumServlet extends HttpServlet {
         parametr( req, resp, true );
     }
 
-    public void TableChat(HttpServletResponse resp, String nick) throws IOException {
-        PrintWriter out = resp.getWriter();
-        out.println( "<h3>Ваш ник - " + nick + "<h3>" );
-        out.println( "Сообщения: " );
+    private void TableChat(HttpServletRequest req,HttpServletResponse resp, String nick) throws IOException, ServletException {
+        req.getServletContext().setAttribute("mes", messages);
+        req.getServletContext().setAttribute("nick", nick);
 
-        for (int i = 0; i < messages.size(); i++) {
-            out.println( "<h4>" + messages.get( i ).getAuthor() +
-                    " : " + messages.get( i ).getText() + " " + messages.get( i ).getDateForum() + "<h4>" );
-        }
-        out.println( "<hr>" );
-        out.println( "<form action=\"/forum\" method=\"post\">" );
-        out.println( "<h4>" + nick + " : </h4>" );
-        out.println( "<input type=\"text\" name=\"messageParam\"/>" );
-        out.println( "<input type=\"hidden\" value=\"" + nick + "\" name=\"nickname\"/>" );
-        out.println( "<input type=\"submit\" value=\"send\"/>" );
-        out.println( "</form>" );
-        out.println( "<hr>" );
+        req.getRequestDispatcher("/MessagerChat.jsp").forward(req, resp);
+
     }
 
-    public void parametr(HttpServletRequest req, HttpServletResponse resp, boolean method) throws IOException {
+    private void parametr(HttpServletRequest req, HttpServletResponse resp, boolean method) throws IOException, ServletException {
         String nick = req.getParameter( "nickname" );
         String message = req.getParameter( "messageParam" );
         Message date = new Message();
 
-        if (method == true) {
-            if (com.belhard.utils.StringUtils.isBlank( message ) == false) {
+        if (method) {
+            if (!com.belhard.utils.StringUtils.isBlank( message )) {
 
                 messages.add( 0, new Message( nick, message, date.getDateForum() ) );
             }
         }
-        TableChat( resp, nick );
+        TableChat(req, resp, nick );
     }
 }
--- web/MessagerChat.jsp
+++ web/MessagerChat.jsp
@@ -5,12 +5,35 @@
   Time: 18:09
   To change this template use File | Settings | File Templates.
 --%>
+
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core"
+           prefix="c" %>
 <html>
 <head>
+
     <title>Chat</title>
 </head>
 <body>
+<h3>Ваш ник - ${nick}</h3>
+
+Сообщения:
+<table>
+    <c:forEach var="message" items="${mes}">
+        <tr>
+            <td> <c:out value="${message.author}"/> </td>
+            <td> <c:out value="${message.text}"/> </td>
+            <td> <c:out value="${message.dateForum}"/> </td>
+        </tr>
+    </c:forEach>
+</table>
+
+
+<form action="forum" method="post">
+    <input type="text" name="messageParam">
+    <input type="hidden" value="${nick}" name="nickname">
+    <input type="submit" value="send">
+</form>
 
 </body>
 </html>
